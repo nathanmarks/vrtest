4051412de78b50bd40aff8f7a1f44a6745770d41
nathanmarks <info@nathanmarks.io>
2017-02-04 21:41:48 -0500

    Fix 2nd integration test

--- test/integration/run.test.js
+++ test/integration/run.test.js
@@ -6,9 +6,30 @@ import proxyquire from 'proxyquire';
 import run from '../../src/server/run';
 import buildFixture from '../fixtures/build';
 
-describe.only('integration: server/run', () => {
+describe('integration: server/run', () => {
   let consoleLog;
   let jsonReporter;
+
+  function createDockerConfig(tests) {
+    return {
+      tests,
+      testUrl: process.env.DOCKER_TEST_URL || 'http://10.200.10.1:3090',
+      storage: {
+        baseline: 'test/fixtures/simple/baseline',
+        output: 'tmp/output',
+      },
+      selenium: {
+        server: 'http://127.0.0.1:4444/wd/hub',
+      },
+      profiles: [{
+        name: 'chrome',
+        desiredCapabilities: {
+          browserName: 'chrome',
+        },
+      }],
+      reporters: [jsonReporter],
+    };
+  }
 
   beforeEach(() => {
     consoleLog = spy();
@@ -25,25 +46,7 @@ describe.only('integration: server/run', () => {
     return buildFixture('simple/tests.js')
       .then((stats) => {
         const tests = stats.compilation.assets['tests.js'].existsAt;
-
-        const config = {
-          tests,
-          testUrl: process.env.DOCKER_TEST_URL || 'http://10.200.10.1:3090',
-          storage: {
-            baseline: 'test/fixtures/simple/baseline',
-            output: 'tmp/output',
-          },
-          selenium: {
-            server: 'http://127.0.0.1:4444/wd/hub',
-          },
-          profiles: [{
-            name: 'chrome',
-            desiredCapabilities: {
-              browserName: 'chrome',
-            },
-          }],
-          reporters: [jsonReporter],
-        };
+        const config = createDockerConfig(tests);
 
         return run(config);
       })
@@ -56,36 +59,24 @@ describe.only('integration: server/run', () => {
       });
   });
 
-  // it('should fail the second test', function () {
-  //   this.timeout(Infinity);
+  it('should fail the second test', function () {
+    this.timeout(Infinity);
 
-  //   return buildFixture('simple/tests-broken.js')
-  //     .then((stats) => {
-  //       const tests = stats.compilation.assets['tests.js'].existsAt;
+    return buildFixture('simple/tests-broken.js')
+      .then((stats) => {
+        const tests = stats.compilation.assets['tests.js'].existsAt;
+        const config = createDockerConfig(tests);
 
-  //       const config = {
-  //         tests,
-  //         storage: {
-  //           baseline: 'test/fixtures/simple/baseline',
-  //           output: 'tmp/output',
-  //         },
-  //         profiles: [{
-  //           name: 'phantomjs',
-  //           browser: 'phantomjs',
-  //         }],
-  //         reporters: [jsonReporter],
-  //       };
-
-  //       return run(config);
-  //     })
-  //     .then(() => {
-  //       const results = JSON.parse(consoleLog.args[0][0]);
-  //       assert.strictEqual(results.total, 2);
-  //       assert.strictEqual(results.passes, 1);
-  //       assert.strictEqual(results.failures, 1);
-  //       assert.strictEqual(results.tests[0].passed, true);
-  //       assert.strictEqual(results.tests[1].passed, false);
-  //       return null;
-  //     });
-  // });
+        return run(config);
+      })
+      .then(() => {
+        const results = JSON.parse(consoleLog.args[0][0]);
+        assert.strictEqual(results.total, 2);
+        assert.strictEqual(results.passes, 1);
+        assert.strictEqual(results.failures, 1);
+        assert.strictEqual(results.tests[0].passed, true);
+        assert.strictEqual(results.tests[1].passed, false);
+        return null;
+      });
+  });
 });
