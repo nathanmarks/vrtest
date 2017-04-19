f3c2ac005b3cbed0321249acb194a6df7ca35664
Nathan Marks <info@nathanmarks.io>
2017-04-19 11:05:42 -0400

    Update selenium-webdriver and re-enable FF tests

--- test/integration/firefox.test.js
+++ test/integration/firefox.test.js
@@ -1,82 +1,83 @@
 // @flow
 
-// import { assert } from 'chai';
-// import { spy } from 'sinon';
-// import proxyquire from 'proxyquire';
-// import run from '../../src/server/run';
-// import buildFixture from '../fixtures/build';
+import { assert } from 'chai';
+import { spy } from 'sinon';
+import proxyquire from 'proxyquire';
+import run from '../../src/server/run';
+import { readConfig } from '../../src/server/utils/config';
+import buildFixture from '../fixtures/build';
 
-// describe('integration: firefox', () => {
-//   let consoleLog;
-//   let jsonReporter;
+describe('integration: firefox', () => {
+  let consoleLog;
+  let jsonReporter;
 
-//   function createDockerConfig(tests) {
-//     return {
-//       tests,
-//       testUrl: process.env.DOCKER_TEST_URL || 'http://10.200.10.1:3090',
-//       storage: {
-//         baseline: 'test/fixtures/simple/baseline',
-//         output: 'tmp/output',
-//       },
-//       selenium: {
-//         server: 'http://127.0.0.1:4444/wd/hub',
-//       },
-//       profiles: [{
-//         name: 'firefox',
-//         desiredCapabilities: {
-//           browserName: 'firefox',
-//         },
-//       }],
-//       reporters: [jsonReporter],
-//     };
-//   }
+  function createDockerConfig(tests) {
+    return readConfig({
+      tests,
+      testUrl: process.env.DOCKER_TEST_URL || 'http://10.200.10.1:3090',
+      storage: {
+        baseline: 'test/fixtures/simple/baseline',
+        output: 'tmp/output',
+      },
+      selenium: {
+        server: 'http://127.0.0.1:4444/wd/hub',
+      },
+      profiles: [{
+        name: 'firefox',
+        desiredCapabilities: {
+          browserName: 'firefox',
+        },
+      }],
+      reporters: [jsonReporter],
+    });
+  }
 
-//   beforeEach(() => {
-//     consoleLog = spy();
-//     jsonReporter = proxyquire('../../src/server/reporters/json', {
-//       './utils/logger': {
-//         log: consoleLog,
-//       },
-//     }).default;
-//   });
+  beforeEach(() => {
+    consoleLog = spy();
+    jsonReporter = proxyquire('../../src/server/reporters/json', {
+      './utils/logger': {
+        log: consoleLog,
+      },
+    }).default;
+  });
 
-//   it('should pass both tests', function () {
-//     this.timeout(Infinity);
+  it('should pass both tests', function () {
+    this.timeout(Infinity);
 
-//     return buildFixture('simple/tests.js')
-//       .then((stats) => {
-//         const tests = stats.compilation.assets['tests.js'].existsAt;
-//         const config = createDockerConfig(tests);
+    return buildFixture('simple/tests.js')
+      .then((stats) => {
+        const tests = stats.compilation.assets['tests.js'].existsAt;
+        const config = createDockerConfig(tests);
 
-//         return run(config);
-//       })
-//       .then(() => {
-//         const results = JSON.parse(consoleLog.args[0][0]);
-//         assert.strictEqual(results.total, 2);
-//         assert.strictEqual(results.passes, 2);
-//         assert.strictEqual(results.failures, 0);
-//         return null;
-//       });
-//   });
+        return run(config);
+      })
+      .then(() => {
+        const results = JSON.parse(consoleLog.args[0][0]);
+        assert.strictEqual(results.total, 2);
+        assert.strictEqual(results.passes, 2);
+        assert.strictEqual(results.failures, 0);
+        return null;
+      });
+  });
 
-//   it('should fail the second test', function () {
-//     this.timeout(Infinity);
+  it('should fail the second test', function () {
+    this.timeout(Infinity);
 
-//     return buildFixture('simple/tests-broken.js')
-//       .then((stats) => {
-//         const tests = stats.compilation.assets['tests.js'].existsAt;
-//         const config = createDockerConfig(tests);
+    return buildFixture('simple/tests-broken.js')
+      .then((stats) => {
+        const tests = stats.compilation.assets['tests.js'].existsAt;
+        const config = createDockerConfig(tests);
 
-//         return run(config);
-//       })
-//       .then(() => {
-//         const results = JSON.parse(consoleLog.args[0][0]);
-//         assert.strictEqual(results.total, 2);
-//         assert.strictEqual(results.passes, 1);
-//         assert.strictEqual(results.failures, 1);
-//         assert.strictEqual(results.tests[0].passed, true);
-//         assert.strictEqual(results.tests[1].passed, false);
-//         return null;
-//       });
-//   });
-// });
+        return run(config);
+      })
+      .then(() => {
+        const results = JSON.parse(consoleLog.args[0][0]);
+        assert.strictEqual(results.total, 2);
+        assert.strictEqual(results.passes, 1);
+        assert.strictEqual(results.failures, 1);
+        assert.strictEqual(results.tests[0].passed, true);
+        assert.strictEqual(results.tests[1].passed, false);
+        return null;
+      });
+  });
+});
